5a88f741b2ec1518f7dd3a39d723810312f21c93
mdazar1991 <azare201@gmail.com>
2026-02-01 14:13:41 +0530

    stock-logic-pur-init

--- Dao/PurchaseDao.py
+++ Dao/PurchaseDao.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 from operator import and_, or_
 import traceback
 from flask import jsonify
@@ -6,6 +7,8 @@ from models.purchase.purchase_details_model import PurchaseDetails
 from models.purchase.purchase_model import PurchaseMaster
 from models.response_model import ResponseModel
 from extensions import db
+from models.stock_ledger import StockLedger
+from models.stock_model import StockMaster
 from sqlalchemy import func, desc
 purchaseEntries = []
 
@@ -40,6 +43,8 @@ def save_purchase_dao(purchaseData):
         
         for p in purchaseData.get("purchaseDetails", []):
             details = PurchaseDetails(**PurchaseDetails.map_purchase_detail_data_to_model(p))
+            update_stock_master(details,p)  
+            update_stock_ledger(details,p)                     
             purchase.purchaseDetails.append(details)
         
         db.session.commit()
@@ -58,3 +63,32 @@ def save_purchase_dao(purchaseData):
         message="OOPS! Something went wrong"
         )
         return jsonify(res.__dict__)
+    
+def update_stock_master(details,p):
+     # Get existing stock row for item
+            stock = StockMaster.query.filter(
+                StockMaster.item_id == details.item_id
+            ).with_for_update().first()
+
+            if stock:
+                # Update existing stock
+                stock.current_qty = (stock.current_qty or Decimal('0')) + Decimal(details.purchase_qty)
+            else:
+                # Create new stock row
+                stock = StockMaster(
+                    **StockMaster.map_stock_data_to_model(p)
+                )      
+                stock.current_qty = details.purchase_qty
+                db.session.add(stock)  
+
+def update_stock_ledger(details,p):
+     
+                stock_txn = StockLedger(
+                    **StockLedger.map_stock_txn_data_to_model(p)
+                )      
+
+                stock_txn.referenceId = details.purchase_id
+                stock_txn.txn_type = "PURCHASE"
+                stock_txn.qty_in = details.purchase_qty
+                stock_txn.rate = details.purchase_price
+                db.session.add(stock_txn)  
